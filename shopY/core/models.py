5a3d86c8dc28cadc61d35498ec176542f4302069
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-20 17:19:21 -0400

    Created a model for items and registered it

--- shopY/core/models.py
+++ shopY/core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -12,5 +13,14 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
-    
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_image', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)    
         
